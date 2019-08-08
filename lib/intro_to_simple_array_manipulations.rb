4c314a005fa1c63f49815bffd28a93b46b04f3c9
Martin Milius <martin.milius@gmail.com>
2019-08-08 01:32:07 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,8 +4,8 @@ def using_push(colors_in_the_rainbow, next_color)
   colors_in_the_rainbow.push(next_color)
 end
 
-def using_unshift (array, string)
-array = ["stuff", "more stuff"]
-string = "less stuff"
-array.unshift(string)
+def using_unshift (@updated_array.first, @new_neighborhood)
+@updated_array.first = ["stuff", "more stuff"]
+@new_neighborhood = "Staten Island"
+@updated_array.first.unshift(@new_neighborhood)
 end
