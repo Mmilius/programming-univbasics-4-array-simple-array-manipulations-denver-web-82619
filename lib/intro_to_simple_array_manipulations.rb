45f65122683e3043e4dce7463a76a0fe36fcbc78
Martin Milius <martin.milius@gmail.com>
2019-08-08 01:29:29 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,3 +3,9 @@ def using_push(colors_in_the_rainbow, next_color)
   next_color = "violet"
   colors_in_the_rainbow.push(next_color)
 end
+
+def using_unshift (array, string)
+array = ["stuff", "more stuff"]
+string = "less stuff"
+array.unshift(string)
+end
